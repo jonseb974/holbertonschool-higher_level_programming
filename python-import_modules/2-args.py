d6d22a3c39493fe015a2f1045203b93443b26760
jonseb974 <5151@holbertonschool.com>
2022-09-09 16:59:20 +0200

    test1_2-args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-def num(*args):
-    if num == 1:
-        print("{} agument.", len(args))
+if __name__ == "__main__":
+    import sys
+
+    num = len(sys.argv)-1
+    if num == 0:
+        print("{} agument.")
+    elif num == 1:
+        print("{} arguments.")
     else:
-        print("{} arguments.", len(args))
+        print("{} arguments:".format(count))
+        for i in range(num):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
