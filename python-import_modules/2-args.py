784476e00144e090ca68a997c5275ca72d17895a
jonseb974 <5151@holbertonschool.com>
2022-09-09 17:08:25 +0200

    test5_2-args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
 
     num = len(sys.argv)-1
     if num == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif num == 1:
-        print("1 argument.")
+        print("1 argument:")
     else:
         print("{} arguments:".format(num))
     for i in range(num):
