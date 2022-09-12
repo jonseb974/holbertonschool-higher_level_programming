5e2428e256e3948fd629bed5cdf4767fc16f3001
jonseb974 <5151@holbertonschool.com>
2022-09-12 11:18:37 +0200

    update4_replace_in_list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # 2-Replace element
+
+
 def replace_in_list(my_list, idx, element):
 
     if idx < 0 and idx > (len(my_list)):
         return None
-
 
     if idx >= 0 and idx < (len(my_list)):
         my_list[idx] = element
