3dd5ac1479454507c8ef39b9c59f30a81200d75b
jonseb974 <5151@holbertonschool.com>
2022-09-12 13:38:15 +0200

    test4no_c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 # 5. Can you C me now?
 
 def no_c(my_string):
-    copy = [i for i in (my_string)) if i != 'c' and != 'C']
+    copy = [i for i in (my_string) if i != 'c' and != 'C']
     return("".join(copy))
