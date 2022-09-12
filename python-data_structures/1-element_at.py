8f8e928990d76c8a18ab39f26f66e8ac4c1be60d
jonseb974 <5151@holbertonschool.com>
2022-09-12 11:02:02 +0200

    test2_task1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 1.Secure access to an element in list.
 def element_at(my_list, idx):
-    if idx < 0 or idx > (len(list) - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
     return (my_list[idx])
