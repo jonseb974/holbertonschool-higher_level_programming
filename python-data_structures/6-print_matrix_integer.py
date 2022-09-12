30ac15f9f2b757c9b3c3520172cc13c0237aae1f
jonseb974 <5151@holbertonschool.com>
2022-09-12 14:04:43 +0200

    test2-print_matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        for row in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][row], end=""))
-            if row != (len(matrix[i]) - 1):
-                print(val, end=" ")
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j], end=""))
+            if j != (len(matrix[i]) - 1):
+                print("", end=" ")
         print("")
