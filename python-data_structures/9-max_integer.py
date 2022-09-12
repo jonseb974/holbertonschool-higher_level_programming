5efe187fa473e6f0de0e37d7a2841c92613397e5
jonseb974 <5151@holbertonschool.com>
2022-09-12 17:13:09 +0200

    test3_max_integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,11 +3,11 @@
 
 
 def max_integer(my_list=[]):
-    if my_list == "":
+    if len(my_list) == 0:
         return (None)
 
     maxVal = my_list[0]
-    for x in my_list:
-        if x > maxVal:
-            maxVal = x
-        print (maxVal)
+    for i in my_list:
+        if my_list[i] > maxVal:
+            maxVal = my_list[i]
+    return (maxVal)
