b3839faf86490942d35d9d9925048210736cc7ed
jonseb974 <5151@holbertonschool.com>
2022-09-12 11:17:22 +0200

    update3_replace_in_list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -5,6 +5,7 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 and idx > (len(my_list)):
         return None
 
+
     if idx >= 0 and idx < (len(my_list)):
         my_list[idx] = element
 
