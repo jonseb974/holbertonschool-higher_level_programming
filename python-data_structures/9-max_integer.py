b77013123fe99fbc369b019099fec34805fb6544
jonseb974 <5151@holbertonschool.com>
2022-09-12 17:09:05 +0200

    test2_max_integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,10 +4,10 @@
 
 def max_integer(my_list=[]):
     if my_list == "":
-        return None
+        return (None)
 
     maxVal = my_list[0]
     for x in my_list:
         if x > maxVal:
             maxVal = x
-        print maxVal
+        print (maxVal)
