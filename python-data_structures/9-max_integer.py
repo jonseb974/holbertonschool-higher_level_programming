1a23c4f80dbf064d59d3c9cbb42a6f7f3782e11e
jonseb974 <5151@holbertonschool.com>
2022-09-12 17:07:57 +0200

    max_integer

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 9. Find the max
+
+
+def max_integer(my_list=[]):
+    if my_list == "":
+        return None
+
+    maxVal = my_list[0]
+    for x in my_list:
+        if x > maxVal:
+            maxVal = x
+        print maxVal
