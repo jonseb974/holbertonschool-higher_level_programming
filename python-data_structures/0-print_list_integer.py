b43b9604edfd3edd37c16c3b291f123811255fc0
jonseb974 <5151@holbertonschool.com>
2022-09-12 09:39:37 +0200

    update_0-print_list_integer

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 # 0. Print a list of integers
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-        print("{:d}.format(my_list[i])")
+        print("{:d}".format(my_list[i]))
