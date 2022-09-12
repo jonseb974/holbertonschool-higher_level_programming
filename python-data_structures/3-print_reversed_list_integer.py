94f47f09a173a2332e0c637226368964583c67ad
jonseb974 <5151@holbertonschool.com>
2022-09-12 11:36:23 +0200

    testOne3-print

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     if (my_list, list):
-        my_list.rever()
+        my_list.reverse()
 
         for i in range(my_list):
             print("{:d".format(i))
