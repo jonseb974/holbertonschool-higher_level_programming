b7ed8143d4b6c347f7a06bd247ae380be1d90342
jonseb974 <5151@holbertonschool.com>
2022-09-14 16:10:02 +0200

    update3_dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # 7. Update dictionary
 
+
 def update_dictionary(a_dictionary, key, value):
     if key not in a_dictionary:
-       a_dictionary[key] = value
+        a_dictionary[key] = value
     else:
         for i in(a_dictionary):
             if i == key:
