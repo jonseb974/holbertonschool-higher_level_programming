95e354f35ca3c59bc7b30802c4e6d9950bbfe598
jonseb974 <5151@holbertonschool.com>
2022-09-14 20:05:13 +0200

    test2roman_to_int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or roman_string is None):
         return (0)
 
-    rom_dict = {
+    r_dict = {
         "I": 1,
         "V": 5,
         "X": 10,
@@ -17,11 +17,12 @@ def roman_to_int(roman_string):
     result = 0
 
     for i in range(len(roman_string)):
-        if rom_dict.get(roman_string[i], 0) == 0:
+        if r_dict.get(roman_string[i], 0) == 0:
             return (0)
 
-        if (i != (len(roman_string) - 1) and rom_dict[roman_string[i]] < rom_dict[roman_string[i + 1]]):
-            result += rom_dict[roman_string[i]] * -1
+        if (i != (len(roman_string) - 1) and
+                r_dict[roman_string[i]] < r_dict[roman_string[i + 1]]):
+            result += r_dict[roman_string[i]] * -1
         else:
-            result += rom_dict[roman_string[i]]
+            result += r_dict[roman_string[i]]
     return (result)
