07663a90f18f69f6f055390ee703251a5c80bb8e
jonseb974 <5151@holbertonschool.com>
2022-09-14 15:36:40 +0200

    test3print_sorted_dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # 6. Print sorted dictionary
 
+
 def print_sorted_dictionary(a_dictionary):
     for elem in sorted(a_dictionary.keys()):
         print("{:s}: {}".format(elem, a_dictionary[elem]))
