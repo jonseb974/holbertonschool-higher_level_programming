b9afe1fb55f4fe0516716a2dd54af453e7d3f4a6
jonseb974 <5151@holbertonschool.com>
2022-09-14 15:56:10 +0200

    update_dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 7. Update dictionary
+
+def update_dictionary(a_dictionary, key, value):
+    if key not in a_dictionary:
+        key == value
+    else:
+        for i in(a_dictionary):
+            if i == key:
+                a_dictionary[i] == value
+    return a_dictionary
