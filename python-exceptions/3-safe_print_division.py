a1d84a4e61edd39f8d47d364547a1fa6e70b7685
jonseb974 <5151@holbertonschool.com>
2022-09-16 11:45:33 +0200

    testOne_safe_print_division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-#3. Integers division with debug
+# 3. Integers division with debug
 
 def safe_print_division(a, b):
+    try:
+        division = a / b
+    except(TypeError, ZeroDivisionError):
+        division = None
+    finally:
+        print("Inside result: {}".format(division))
+    return(division)
