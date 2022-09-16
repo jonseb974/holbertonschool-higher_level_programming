d19ee8ed92fbd3864dad872c62c82b4fc73c0be3
jonseb974 <5151@holbertonschool.com>
2022-09-16 11:25:11 +0200

    safe_print_first_test

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,5 +2,9 @@
 # 1. Safe printing of an integers list
 
 
-
 def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except(TypeError, ValueError):
+        return(False)
