ebed6681053457690ba686c4fc428ba21e048ff7
jonseb974 <5151@holbertonschool.com>
2022-09-19 16:33:33 +0200

    testsquare_size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class Square that defines a square"""
+
+
+class Square:
+    """square pattern"""
+
+    def __init__(self, size):
+        """initialisation for the square size"""
+        self.__size = size
