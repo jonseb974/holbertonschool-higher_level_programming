ff26c8771880c0de21d81084937bf50bc00a4d6f
jonseb974 <5151@holbertonschool.com>
2022-09-20 14:45:07 +0200

    testnew

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,8 +4,7 @@
 class Square:
     """Class square."""
     def __init__(self, size=0):
-        """
-        Initialisation for a new square
+        """Initialisation for a new square
         Args:
         size (int): size of the square.
         __size: attribute.
