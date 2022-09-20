821c3088f270abffcbefc6f7f62b8944c42255ba
jonseb974 <5151@holbertonschool.com>
2022-09-20 13:44:40 +0200

    trytreesquare

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-class Square:
-    """Represent a square.
-    Initialisation for a new square.
-    Args:
-    size (int): the size of the square.
-    """
 
+
+class Square:
+    """Class square."""
     def __init__(self, size=0):
-        """Initialisation for a new square"""
+        """
+        Initialisation for a new square
+        Args:
+        size (int): size of the square.
+        __size: attribute.
+        """
         self.__size = size
         if (type(self.__size) != int):
             raise TypeError("size must be an integer")
