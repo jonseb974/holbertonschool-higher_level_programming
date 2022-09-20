9d73b5afb251b3ef901b29ad823b066fb12b4b41
jonseb974 <5151@holbertonschool.com>
2022-09-20 10:05:03 +0200

    test4square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 # 2-square
-"""Define a square"""
 
 
 class Square:
+    """
+    Initialisation to create the square
 
-    """Define a square"""
+    Args:
+    size (int): The size of the new_square.
+    """
+
     def __init__(size, size=0):
-        """
-        Initialisation to create the square
-
-        Args:
-        size (int): The size of the new_square.
-
-        """
+        """Initialisation"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
         else size < 0:
-            raise ValueError("size must be >= 0)
-
+            raise ValueError("size must be >= 0")
         self.__size = size
