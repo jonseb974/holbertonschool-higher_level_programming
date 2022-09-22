4aa90b9239dfe8eee43891f77be8f31572e35e1f
jonseb974 <5151@holbertonschool.com>
2022-09-22 21:56:16 +0200

    6_square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -9,20 +9,21 @@ class Square:
         Args:
         size (int): size of the square
         """
-        self.__size = size
+        self.size = size
         sefl.position = position
 
     @property
     def size(self):
         """set the current size of the square."""
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
+        """Set the size value."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("value must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
@@ -31,3 +32,32 @@ class Square:
         return(self.__position)
 
     @position.setter
+    def position(self, value):
+        """Set position"""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """ Returns current square area."""
+        return(sel.__size ** 2)
+
+    def my_print(self):
+        """Print square with '#' character,
+        at position given by position attribute.
+        """
+        if self.__size == 0:
+            print()
+            return
+        for y in range(0, self.__position[1]):
+            print()
+        for i in range(0, self.__size):
+            for x in range(0, self.__position[0]):
+                print(" ", end="")
+            for j in range(0 self.__size):
+                print('#', end="")
+            print()
