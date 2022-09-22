946bf92423356554f7215265d7ee57eb7e525268
jonseb974 <5151@holbertonschool.com>
2022-09-22 15:55:00 +0200

    print_square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 4-print_square.py
+"""
+print a square shape
+using '#' characters
+"""
+
+
+def print_square(size):
+    """ print a square
+    Args:
+    size (int): length and width
+    of the square.
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if (size) < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
