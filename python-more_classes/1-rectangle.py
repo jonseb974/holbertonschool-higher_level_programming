d12ae9cc7d9be6c656fbe1d2489f7af88865a616
jonseb974 <5151@holbertonschool.com>
2022-09-23 22:04:42 +0200

    rect_update

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle:
     @property
     def height(self):
         """set height of the rectangle. """
-        return self.height
+        return self.__height
 
     @height.setter
     def heigth(self, value):
