fb6f8a12778c510ac5c80804e3aba71079a1f477
jonseb974 <5151@holbertonschool.com>
2022-09-25 12:22:18 +0200

    4-print

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -18,5 +18,5 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     for i in range(size):
         for j in range(size):
-            print('#' * size)
-        print()
+            print('#', end='')
+        print("")
