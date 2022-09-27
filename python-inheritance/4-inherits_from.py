b16d637b400ac42e65d5242e7d22896749457777
jonseb974 <5151@holbertonschool.com>
2022-09-27 19:56:14 +0200

    4-inherits

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 4-inherits_from
+"""function that returns True
+if the object is an instance of a class
+"""
+
+
+def inherits_from(obj, a_class):
+    """function that returns True
+    if the object is an instance of a class
+    that inherited (directly or indirectly)
+    from the specified class;
+    otherwise False
+    """
+    return isinstance(obj, a_class) and type(obj) != (a_class)
