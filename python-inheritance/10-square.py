6bf88079bae06dd8cc39242db3857583ce5efa14
jonseb974 <5151@holbertonschool.com>
2022-09-27 22:15:23 +0200

    10-square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# 10-square.py
+"""class Square
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(rectangle):
+    """Square
+    Private instance attribute size.
+    Public method area()
+    """
+    def __init__(self, size):
+        """Instantiation
+        Args:
+            size (int): size of the rectangle.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Method area
+        """
+        return(self.__size ** 2)
