ed577598eadb30c62063ebb29f0f5f0e345041bf
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-09-29 20:56:07 +0200

    3-to_json_string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 # 3-to_json_string.py
+
+import json
+
+"""function that returns the JSON
+representation of an object (string)
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """function that returns the JSON
+    Args:
+        my_obj: string
+    """
+    return json.dumps(my_obj)
