fb61ed5abcf01dbe03045499c738616edf1289d1
jonseb974 <5151@holbertonschool.com>
2022-09-29 16:47:28 +0200

    1_write_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-# 1-write_file.py 
-"""function that writes a string to a text file
+# 1-write_file.py
+"""function that writes a string to a text file.
 """
 
+
+def write_file(filename="", text=""):
+    """function that writes a string to a text file
+    Args:
+        * filename: Name of the file.
+        * text: String.
+    """
+    with open(filename, 'w+') as f:
+        return f.write(text)
