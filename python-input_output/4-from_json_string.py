9bcf34f50156ff1886e3cf8d7917194c252dc089
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-09-29 21:32:11 +0200

    4_from_json_string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 4-from_json_string.py
+"""Write a function that returns an object
+(Python data structure) represented by a JSON string
+"""
+
+
+def from_json_string(my_str):
+"""
+"""
