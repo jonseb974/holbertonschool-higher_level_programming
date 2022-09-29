a894bc57454766edd34c96b0afd249cfb4ee0ace
jonseb974 <5151@holbertonschool.com>
2022-09-29 21:57:32 +0200

    update3-to_json_string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -7,13 +7,6 @@ the JSON representation of an object (string)
 
 import json
 
-"""function that returns the JSON
-representation of an object (string)
-"""
-
-
-import json
-
 
 def to_json_string(my_obj):
     """function that returns the JSON
