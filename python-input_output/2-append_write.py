b0c2d830734b764d3c05db43444c850457d7d44e
jonseb974 <5151@holbertonschool.com>
2022-09-29 17:02:57 +0200

    2_append_write

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 # 2-append_write.py
+"""function that appends a string
+at the end of a text file that writes
+a string to a text file.
+"""
 
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file.
+        Args:
+            filename: Name of the file.
+            text: String.
+    """
+    with open(filename, 'a+') as f:
+        return f.write(text)
