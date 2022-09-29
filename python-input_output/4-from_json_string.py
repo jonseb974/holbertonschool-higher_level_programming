4129dcd2325f47a75d55b1f2c49293da11fe142a
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-09-29 21:37:20 +0200

    4from

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -4,7 +4,12 @@
 (Python data structure) represented by a JSON string
 """
 
+import json
+
 
 def from_json_string(my_str):
-"""
-"""
+    """function that returns an object
+    Args:
+        str: string
+    """
+    return json.loads(my_str)
