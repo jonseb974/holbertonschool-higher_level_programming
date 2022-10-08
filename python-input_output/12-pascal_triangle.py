25607ca64208d60282b32d1ce73177eb6dbd7a2c
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-10-08 15:38:07 +0200

    test5-pascal_triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
 # 12-pascal_triangle.py
-""" Defines a class Student. """
+""" Defines a Pascal's triangle. """
 
 
-class Student:
-    """ Represent a class student."""
-
-    def __init__(self, first_name, last_name, age):
+def pascal_triangle(n):
         """
-        Initialisation.
+        Represent a pascal triangle of size n.
+        Return a list of integers representing a triangle.
+        """
+        if n <= 0:
+            return[]
 
-        Args:
-            first_name (str): First name of the student.
-            last_name (str): Last name of the student.
-            age (int): Age of the student.
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """
-        Get dictionary representation of a student
-        Args:
-            attrs (list): Some attributes.
-        """
-        if(type(attrs) == list and
-           all(type(i) == str for i in attrs)):
-            return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
-        return self.__dict__
+        triangles = [[1]]
+        while len(triangles) != n:
+            tri = triangles[-1]
+            tmp = [1]
+            for i in range(len(tri) - 1):
+                tmp.append(tri[i] + tri[i + 1])
+            tmp.append(1)
+        return triangles
