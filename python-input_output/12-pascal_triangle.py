ac6fdd5057723f4babd8d67089bc88ecfc077408
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-10-08 15:06:31 +0200

    12-pascal_triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,3 +1,29 @@
 #!/usr/bin/python3
 # 12-student.py
 
+
+class Student:
+    """Represent a class student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initialisation.
+
+        Args:
+            first_name (str): First name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get dictionary representation of a student
+        Args:
+            attrs (list): Some attributes.
+        """
+        if(type(attrs) == list and
+           all(type(i) == str for i in attrs)):
+            return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
+        return self.__dict__
