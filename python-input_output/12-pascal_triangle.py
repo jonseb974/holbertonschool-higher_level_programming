12b4dab03dfa730c250ad6765b0783ee5e9d4839
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-10-08 15:40:48 +0200

    test6-pascal_triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -18,4 +18,5 @@ def pascal_triangle(n):
             for i in range(len(tri) - 1):
                 tmp.append(tri[i] + tri[i + 1])
             tmp.append(1)
+            triangles.append(tmp)
         return triangles
