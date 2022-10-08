68f7cc744be23194c532c3b5343970c104880444
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-10-08 16:53:49 +0200

    base_py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,3 +1,28 @@
 #!/usr/bin/python3
 # base.py
 """ Write the first class Base."""
+import json
+import csv
+import turtle
+
+
+class Base:
+    """Represent the base model.
+
+    Attributes:
+        __nb_objects (int):Nb of instantiated Bases.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialisation.
+
+        Args:
+            id (int): Identtity of the base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
