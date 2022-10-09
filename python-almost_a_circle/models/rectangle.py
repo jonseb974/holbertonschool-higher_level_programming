bf69fdb0d409a548095e7b591e40058234e0bb7e
jonad sebastien <105843973+jonseb974@users.noreply.github.com>
2022-10-09 20:21:22 +0200

    test8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -153,18 +153,6 @@ class Rectangle(Base):
             raise ValueError("width must be > 0")
         self.__width = value
 
-    @height.setter
-    def height(self, value):
-        """
-        Set height attributes.
-        define height of the rectangle.
-        """
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
     @property
     def height(self):
         """
