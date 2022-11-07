7f834cce19aa0392da04931c998ac95c77f19b27
jonseb974 <5151@holbertonschool.com>
2022-11-07 14:27:33 +0100

    file5-test2

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -2,4 +2,4 @@
 -- unique_id description: id INT with the default value 1 and,
 -- must be unique name VARCHAR(256) If the table unique_id already exists, your script should not fail.
 CREATE TABLE IF NOT EXISTS unique_id (
-	id INT (1) NOT NULL UNIQUE, name VARCHAR(256) NOT NULL);
+	id INT NOT NULL DEFAULT 1 UNIQUE, name VARCHAR(256));
