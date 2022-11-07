a632a3037164530bd89c1564c7495da305a39d81
jonseb974 <5151@holbertonschool.com>
2022-11-07 18:36:11 +0100

    file_6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,10 +1,9 @@
 --  a script that creates the database hbtn_0d_usa and the table states.
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS 'states'(
-	PRIMARY KEY('id'),
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+	PRIMARY KEY(`id`),
 	UNIQUE('id'),
-	'id' INT NOT NULL AUTO_INCREMENT,
-	'name' VARCHAR(256) NOT NULL
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(256) NOT NULL
 );
