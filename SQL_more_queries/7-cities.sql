a65653c33c72f270c5eb0903a07c1e26963d85ec
jonseb974 <5151@holbertonschool.com>
2022-11-07 19:06:49 +0100

    file7-cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	PRIMARY KEY(id),
+	UNIQUE(id),
+	state_id INT NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
+	FOREIGN KEY(state_id),
+	REFERENCES states(id)
+	name VARCHAR(256) NOT NULL
+);
