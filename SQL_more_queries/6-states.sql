93471aabc84d52f3aa11f44354416c12143cabb7
jonseb974 <5151@holbertonschool.com>
2022-11-07 16:03:16 +0100

    file6-new4

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -5,7 +5,7 @@
 -- If the database hbtn_0d_usa already exists, your script should not fail,
 --If the table states already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.'states'(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	PRIMARY KEY('id'),
 	'id' INT NOT NULL AUTO_INCREMENT UNIQUE,
 	'name' VARCHAR(256) NOT NULL
