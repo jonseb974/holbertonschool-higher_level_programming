f66f84c535fdfcd2013e48dd1e973099a8c509e0
jonseb974 <5151@holbertonschool.com>
2022-11-07 13:22:44 +0100

    file2-test3

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,6 @@
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2
-CREATE USER 'user_0d_2'@'localhost'
-WITH PASSWORD = 'user_0d_2_pwd';
+CREATE USER 'user_0d_2'@'localhost'IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT TO 'user_02_'@'localhost';
 
