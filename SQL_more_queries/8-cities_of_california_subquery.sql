8b897f3b38640aa28543d30ddb5441b68bf1f94a
jonseb974 <5151@holbertonschool.com>
2022-11-07 20:39:57 +0100

    file8-cities_sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,13 +1,2 @@
 -- a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa; 
-CREATE TABLE IF NOT EXISTS cities(
-	state_id INT NOT NULL,
-	id INT UNIQUE AUTO_INCREMENT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY(id),
-	FOREIGN KEY(state_id) REFERENCES states(id)
-	WHERE name = 'California';
-	ORDER BY cities.id DESC
-);
-
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id;
