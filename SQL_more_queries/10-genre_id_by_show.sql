240ee5d729b90d55e76b8301d96ed1120bfe3bfd
jonseb974 <5151@holbertonschool.com>
2022-11-08 10:45:35 +0100

    file10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,6 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT 
+SELECT s.'title', g.'genre_id'
+FROM 'tv_shows' AS 's'
+INNER JOIN 'tv_show_id' AS 'g'
+ON s.'id' = g.'show_id'
+ORDER BY  s.'title', g.'genre_id';
