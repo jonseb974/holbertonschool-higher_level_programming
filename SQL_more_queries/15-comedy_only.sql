7ee774f9ff31f159077c793c0ddc4c63ea13bbd6
jonseb974 <5151@holbertonschool.com>
2022-11-08 17:43:25 +0100

    file15-comedy_only2

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -6,8 +6,8 @@ FROM tv_shows
 INNER JOIN tv_show_genres
 	ON title.id = tv_show_id
 
-INNER JOIN tv_genres
+INNER JOIN gtv_genres
 	ON tv_genre.id = title.genre_id 
 
-WHERE tv_genre.name = "Comedy"
+WHERE genre.name = "Comedy"
 ORDER BY title ASC;
