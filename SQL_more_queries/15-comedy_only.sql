744461a6a6a0de714fb55e5b9724ffc91b50d54d
jonseb974 <5151@holbertonschool.com>
2022-11-09 14:42:18 +0100

    file15-comedy2

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,13 +1,11 @@
 -- a script that lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT tv_shows.title
+SELECT tv_shows.title FROM tv_shows
 
-FROM tv_shows
-
-INNER JOIN tv_show_genres
+JOIN tv_show_genres
 	ON tv_shows.id = tv_show_genres.show_id
 
-INNER JOIN tv_genres
-	ON tv_genres.genre_id = tv_genres_id 
+JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres_id 
 
 WHERE tv_genre.name = "Comedy"
 
