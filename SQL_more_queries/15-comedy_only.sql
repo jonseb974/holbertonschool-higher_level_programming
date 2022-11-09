0032752651b40a95b2323b72149e398e4657e7a9
jonseb974 <5151@holbertonschool.com>
2022-11-09 15:29:41 +0100

    file15-update2

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -9,6 +9,6 @@ JOIN tv_show_genres
 JOIN tv_genres
 	ON tv_show_genres.genre_id = tv_genres.id 
 
-WHERE tv_show_genres.name = 'Comedy'
+WHERE tv_genres.name = 'Comedy'
 
 ORDER BY tv_shows.title ASC;
