f36a8c91f93da17237e6bbd9bcf5964dde403059
jonseb974 <5151@holbertonschool.com>
2022-11-09 14:38:07 +0100

    file15-comedy

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -4,10 +4,11 @@ SELECT tv_shows.title
 FROM tv_shows
 
 INNER JOIN tv_show_genres
-	ON tv_shows.id = tv_show_genres.id
+	ON tv_shows.id = tv_show_genres.show_id
 
 INNER JOIN tv_genres
-	ON tv_genres.id = tv_show_genres_id 
+	ON tv_genres.genre_id = tv_genres_id 
 
-WHERE tv_shows.name = "Comedy"
+WHERE tv_genre.name = "Comedy"
+
 ORDER BY tv_shows.title ASC;
