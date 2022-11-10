232274b814c911e13921d75b818ea7c859488391
jonseb974 <5151@holbertonschool.com>
2022-11-10 21:43:30 +0100

    file1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""a script that lists all states
+   with a name starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306, charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    [print(states) for state in cur.fetchall()]
+    cur.close()
+    db.close()
