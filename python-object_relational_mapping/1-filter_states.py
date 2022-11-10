529c7b45516437b7bd1650d46aa3b3d546b54061
jonseb974 <5151@holbertonschool.com>
2022-11-10 21:45:52 +0100

    file1_test3

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
                          db=argv[3], port=3306, charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    [print(states) for state in cur.fetchall()]
+    [print(state) for state in cur.fetchall()]
     cur.close()
     db.close()
