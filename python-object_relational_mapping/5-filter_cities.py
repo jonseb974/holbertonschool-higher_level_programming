33fa136c689654eedb55fb914d72717ef8b91ae8
jonseb974 <5151@holbertonschool.com>
2022-11-11 20:30:08 +0100

    file5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 5-filter_cities.py
+"""script that takes in an argument and displays all values in the states
+    table of hbtn_0e_0_usa where name matches the argument.
+    This time, write one that is safe from MySQL injections.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306, charset="utf8")
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ")
+    [print(state) for state in cur.fetchall() if state[1] == argv[4]]
+
+    cur.close()
+    db.close()
