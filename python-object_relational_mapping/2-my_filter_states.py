f79a1a149620e444dbca920e2ccafff943a31248
jonseb974 <5151@holbertonschool.com>
2022-11-11 19:36:31 +0100

    file2_test6

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,10 +13,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states WHERE name='{}'".format(argv[4]))
-    query_rows = cur.fetchall()
-
-    for row in query_rows:
-        print(row)
+    [print(state) for state in cur.fetchall()]
 
     cur.close()
     db.close()
