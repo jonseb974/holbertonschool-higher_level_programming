55a1d7af9465d726e0b9e7434e8c7675a16a3258
jonseb974 <5151@holbertonschool.com>
2022-11-11 18:50:29 +0100

    file2_filter6

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
-""" 2-my_filter_states.py
-    a script that takes in an argument and displays all values in
-    the states table of hbtn_0e_0_usa where name matches the argument.
+# !/usr/bin/python3
+"""
+2-my_filter_states.py
+a script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.
 """
 
 import MySQLdb
