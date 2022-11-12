4392b3d62c6e5472ba7d10aebade729014c757c3
jonseb974 <5151@holbertonschool.com>
2022-11-12 21:15:01 +0100

    fil10-modeltest2

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -12,17 +12,15 @@ from sys import argv
 if __name__ == "__main__":
     aux = 0
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1],
-        argv[2],
-        argv[3]),
-        pool_pre_ping=True)
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     session = Session(engine)
 
     state = session.query(State).filter(State.name == argv[4]).first()
 
-    if state:
-        print("{}".format(state.id))
-    else:
+    if state is None:
         print("Not found")
+    else:
+        print("{:d}".format(state.id))
     session.close()
