bda98140c191e0a342c13c072c3cf7a9c3c8526f
jonseb974 <5151@holbertonschool.com>
2022-11-12 16:34:44 +0100

    file7-model_state4

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,7 +12,7 @@ def allstates():
     """List all the states.
     """
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
