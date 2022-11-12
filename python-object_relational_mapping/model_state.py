3d2dbaa92cc0c4571d71d2dc3a0c65b31be440bf
jonseb974 <5151@holbertonschool.com>
2022-11-12 11:36:13 +0100

    file6_test3

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-# model_state.py
-"""Python file that contains the class definition
-   of a State and an instance Base = declarative_base().
+#!/usr/bin/python3
+"""Start link class to table in database
 """
-
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
