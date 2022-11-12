666500fdc3a31c1f2b482e5eaf96d51a7b7c8070
jonseb974 <5151@holbertonschool.com>
2022-11-12 10:58:08 +0100

    file5-filter

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,6 +8,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    new_list = []
     db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306, charset="utf8")
     cur = db.cursor()
@@ -19,7 +20,11 @@ if __name__ == "__main__":
                  WHERE s.name = %s \
                  ORDER BY c.id ASC;", (argv[4],))
 
-    [print(state) for state in cur.fetchall()]
+    query_row = cur.fetchall()
+
+    for row in query_row:
+        new_list.append(row[0])
+    print(", ".join(new_list))
 
     cur.close()
     db.close()
