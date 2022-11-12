1d953c9ae44a04a96445961fba46b20c24197e9f
jonseb974 <5151@holbertonschool.com>
2022-11-12 22:16:03 +0100

    file12-model

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 12-model_state_update_id_2.py
+"""Script that prints the State object with the name
+passed as argument from the database  hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    qry = session.query(State).filter_by(id=2).first()
+    qry.mame = "New Mexico"
+    session.commit()
+    session.close()
