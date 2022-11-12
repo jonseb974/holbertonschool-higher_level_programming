a6c101c22286eae7a597fec10c9ba17938a1a2fa
jonseb974 <5151@holbertonschool.com>
2022-11-12 19:13:19 +0100

    file8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,15 +8,12 @@ import sys
 
 
 if __name__ == "__main__":
-    firstStates()
-    """List all the states.
-    """
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1],
         sys.argv[2],
         sys.argv[3]),
         pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     session = Session(engine)
     try:
