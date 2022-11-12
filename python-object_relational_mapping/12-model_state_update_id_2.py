01a4a020ea54dc5141f023b7e0d32a30a38e6a40
jonseb974 <5151@holbertonschool.com>
2022-11-12 22:35:14 +0100

    file12-modeltest5

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,13 +10,13 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
     session = Session(engine)
 
-    qry = session.query(State).filter_by(id==2).first()
+    qry = session.query(State).filter_by(id=2).first()
     qry.mame = 'New Mexico'
     session.commit()
     session.close()
