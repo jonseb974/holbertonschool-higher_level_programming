4ec58daa08d24cde9aebee33bfc5f74e872d9dd9
jonseb974 <5151@holbertonschool.com>
2022-11-12 16:49:10 +0100

    file7-modeltest

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -17,12 +17,11 @@ def allstates():
         sys.argv[2],
         sys.argv[3]),
         pool_pre_ping=True)
-
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for State in session.query(State).order_by(State.id).all():
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
 
 
