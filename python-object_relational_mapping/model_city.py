a77ac16d19edce62125bbd392b1cead19cea411a
jonseb974 <5151@holbertonschool.com>
2022-11-12 23:34:22 +0100

    filemodel_citypy

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains the class definition of a City.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Create State class
+    """
+    ___tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
