7a9b1f8af2b71c8aa12844b42a9515ec8936091d
jonseb974 <5151@holbertonschool.com>
2022-11-12 20:38:21 +0100

    file9-model_test3

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -18,12 +18,14 @@ def firstState():
         argv[3]),
         pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.contains("a")))
+
+    states = session.query(State).filter(State.name.contains("a"))
     for state in states:
         print("{}: {}".format(state.id, state.name))
-    session.close()
+        session.close()
 
 
 if __name__ == '__main__':
