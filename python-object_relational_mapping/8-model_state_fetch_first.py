cde1ae269ff8a1f1f8adf9c1466051d1eac50c54
jonseb974 <5151@holbertonschool.com>
2022-11-12 19:31:13 +0100

    file8_test1

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""A script that lists all State objects from the database hbtn_0e_6_usa
+# 8-model_state_fetch_first.py
+"""a script that prints the first State object from the database hbtn_0e_6_usa.
 
 """
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]),
+        argv[1],
+        argv[2],
+        argv[3]),
         pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
