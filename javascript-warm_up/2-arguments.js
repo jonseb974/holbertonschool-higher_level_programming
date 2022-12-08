1e4a77b276229296a15a65c5ab60ccaedea4cbba
jonseb974 <5151@holbertonschool.com>
2022-12-08 15:44:56 +0100

    create file2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+import { argv } from 'node:process';
+
+function enterSomething() {
+	const myVar = prompt("enter some date. ");
+	if (!myVar) {
+		console.log('No argument');
+	} else if {
+		console.log('Argument found');
+	}
+	else
+		console.log('Arguments found')
+	}
+}
