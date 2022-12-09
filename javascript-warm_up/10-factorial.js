ab339093e6f3ac101a7bb9938b526f0f68d9bf69
jonseb974 <5151@holbertonschool.com>
2022-12-09 15:04:07 +0100

    script test file 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-function factorial(num) {
-	if (num > 0) {
-		return factorial(num - 1) * num
-	} else { 
-		return 1;
-	}
+function factorial (num) {
+  if (num > 0) {
+    return factorial(num - 1) * num;
+  } else {
+    return 1;
+  }
 }
 console.log(factorial(process.argv[2]));
