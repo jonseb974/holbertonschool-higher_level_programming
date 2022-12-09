431a5c300200253cf256737db1cae33ad7ff1177
jonseb974 <5151@holbertonschool.com>
2022-12-09 11:45:29 +0100

    file7-multi_c

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 
+let x = 3;
+let string = '';
+for (let i = 0; i < x; i++) {
+	for (let j = 0; j < 1; j++) {
+		string += 'C is fun';
+	}
+	string  += '\n';
+}
+console.log(string);
