ff6c6e43ba6243d83e31323442240c840c33c025
jonseb974 <5151@holbertonschool.com>
2022-12-09 13:32:37 +0100

    file8-square_test

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
+const size = Math.floor(Number(process.argv[2]));
 
+if (isNaN(size)) {
+  console.log('Missing number of occurences');
+} else {
+	for (let j = 0; j < size; j++) {
+		let row = '';
+		for (let i = 0; i < size; i++) {
+			row += 'X';
+			//console.log(row);
+		}
+		console.log(row);
+	}
+}
