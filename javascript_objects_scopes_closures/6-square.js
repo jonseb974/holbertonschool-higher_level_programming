d534b50f7d6bb5296bce1201af8556691ec6bebf
jonseb974 <5151@holbertonschool.com>
2022-12-09 19:22:15 +0100

    createfile6 format issues fixed

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,9 +2,9 @@
 module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
-	    this.print();
+      this.print();
     } else {
-	    for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
