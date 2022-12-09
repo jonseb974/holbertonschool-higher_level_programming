54974115163175c9a7a84e464bfeb7779f83c3b8
jonseb974 <5151@holbertonschool.com>
2022-12-09 13:37:02 +0100

    file8-square_semistandard_update

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,11 +4,11 @@ const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-	for (let j = 0; j < size; j++) {
-		let row = '';
-		for (let i = 0; i < size; i++) {
-			row += 'X';
-		}
-		console.log(row);
-	}
+  for (let j = 0; j < size; j++) {
+    let row = '';
+    for (let i = 0; i < size; i++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
 }
