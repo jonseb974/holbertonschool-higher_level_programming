15c40857bb8d330afa75c240a196f1cbd8f7f9c0
jonseb974 <5151@holbertonstudents.com>
2022-12-12 22:38:53 +0100

    createfile3_starwarstiyle

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (err, response, body) {
+  console.log(err || JSON.parse(body).title);
+});
