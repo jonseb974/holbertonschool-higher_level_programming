5c9923cb39dd187d291a6b6c6e317d3e18729ea1
jonseb974 <5151@holbertonstudents.com>
2022-12-12 21:18:22 +0100

    createFile1-writeme

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-fs = require('fs');
+const fs = require('fs');
 
-fs.writeFile( process.argv[2], 'utf-8', (err) => { if (err) throw err; });
+fs.writeFile(process.argv[2], 'utf-8', (err) => { if (err) throw err; });
