6d28d78ec2cf8bf8c9c8204ae53ed6d4ee679f58
jonseb974 <5151@holbertonstudents.com>
2022-12-13 15:08:51 +0100

    new script file4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-let assert = require('assert'); 
-let count = 0;
-
-fs.readWrite(process.argv[2].each(function() {
-	if ((this).attr(process.argv[3]) == '18')
-		count++;
-	console.log(count);
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
 });
