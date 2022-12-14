886f42ec595adc954fbf34746657062f901c5337
jonseb974 <5151@holbertonstudents.com>
2022-12-14 20:59:54 +0100

    first script testing

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,1 +1,3 @@
-#!/usr/bin/node
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function(data) {
+  $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
+});
