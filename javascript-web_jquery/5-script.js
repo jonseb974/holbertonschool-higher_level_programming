4d1369fe9b4e5ea01e301ad42d20edf5895d3744
jonseb974 <5151@holbertonstudents.com>
2022-12-15 14:01:13 +0100

    update format file5

--- javascript-web_jquery/5-script.js
+++ javascript-web_jquery/5-script.js
@@ -1,3 +1,5 @@
-$('DIV#add_item').click(function() {
-	$('UL.my_list').append('<li>Item</li>');
+const $ = window.$;
+
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
 });
