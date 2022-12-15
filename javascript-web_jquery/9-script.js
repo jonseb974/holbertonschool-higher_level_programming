a187573d754f8a20fced5cd9c8eeee69263bd76f
jonseb974 <5151@holbertonstudents.com>
2022-12-15 14:08:16 +0100

    update format file9

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,5 +1,7 @@
-$('document').ready(function () { 
-  $.get('https://stefanbohacek.com/hellosalut/?lang=fr', function(data) {
+const $ = window.$;
+
+$('document').ready(function () {
+  $.get('https://stefanbohacek.com/hellosalut/?lang=fr', function (data) {
     $('DIV#hello').text(data.hello);
   });
 });
