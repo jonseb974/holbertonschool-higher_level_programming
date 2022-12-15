9b669a47ede6203ca960b27a307e8076d8076bed
jonseb974 <5151@holbertonstudents.com>
2022-12-15 14:04:25 +0100

    update format file7

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,3 +1,5 @@
-$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function(data) {
-	$('DIV#character').text(data.name);
+const $ = window.$;
+
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+  $('DIV#character').text(data.name);
 });
